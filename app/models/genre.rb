852121dd38b4d7e85054542065d975d7c4d268e6
Berry  Semexan <bsemexan@gmail.com>
2018-07-16 04:44:05 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,13 +5,10 @@ class Genre < ActiveRecord::Base
 
   # extend Concerns::Slugifiable::ClassMethods
   # include Concerns::Slugifiable::InstanceMethods
-<<<<<<< HEAD
   def slug
     self.name.downcase.gsub(' ', '-')
   end
   def self.find_by_slug(slug)
     Genre.all.find{ |instance| instance.slug == slug }
   end
-=======
->>>>>>> 6142af312b7f96f48e1db75fbedb448dbf06eec0
 end
