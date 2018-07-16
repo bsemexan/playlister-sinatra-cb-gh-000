1886f6546af19df990703a64115684e18755bd1b
Berry  Semexan <bsemexan@gmail.com>
2018-07-16 04:44:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,6 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, :through => :song_genres
 
-<<<<<<< HEAD
   # extend Concerns::Slugifiable::ClassMethods
   # include Concerns::Slugifiable::InstanceMethods
   def slug
@@ -12,8 +11,4 @@ class Song < ActiveRecord::Base
   def self.find_by_slug(slug)
     Song.all.find{ |instance| instance.slug == slug }
   end
-=======
-  extend Concerns::Slugifiable::ClassMethods
-  include Concerns::Slugifiable::InstanceMethods
->>>>>>> 6142af312b7f96f48e1db75fbedb448dbf06eec0
 end
